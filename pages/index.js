196e2ee14148e555e4e96a805ac19feee93d9a55
Lucas Gabriel <125925164+Luckleir@users.noreply.github.com>
2025-01-11 01:27:56 +0000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>Faz o L, vai ter que declarar o pix.</h1>
-            <h2>Se você é gay, dá uma risadinha xD</h2>
+    return <><h1>Faz o L, vai ter que declarar o pix.</h1><h2>Se voce e gay, da uma risadinha xD</h2></>
 }
 
 export default Home;
