21923b65df4b13c8c9e7d6686044c745cf1d6e1a
Lucas Gabriel <125925164+Luckleir@users.noreply.github.com>
2025-01-15 00:19:37 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <><h1>Faz o L, vai ter que declarar o pix.</h1><h2>Se voce e gay, da uma risadinha xD</h2></>
+    return <><h1>Vai treinar amanhã?</h1></>
 }
 
 export default Home;
