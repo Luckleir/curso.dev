9d44d7e870b766f4bd5b73a2d533021d662448ad
Lucas Gabriel <125925164+Luckleir@users.noreply.github.com>
2025-02-10 04:14:07 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <><h1>Vai treinar amanhã?</h1></>
+  return (
+    <>
+      <h1>Vai treinar amanhã?</h1>
+    </>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
